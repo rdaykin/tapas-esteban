d3f87e9c9f5c92fb241918f1a78a0745c59db790
Rob Daykin <Rob Daykin>
2017-01-13 01:07:45 +0000

    Added basic site framework and carousel to front page.

--- src/main/java/com/tplace/tapasesteban/SpringRootConfig.java
+++ src/main/java/com/tplace/tapasesteban/SpringRootConfig.java
@@ -1,9 +1,13 @@
 package com.tplace.tapasesteban;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({ "com.mkyong.helloworld.service" })
 public class SpringRootConfig {
+	
 }
